8c08c5409054594b86c4921cb4d8d8286892dd56
Haseeb Asif <muhammad-haseeb.asif@electrolux.com>
2023-02-19 14:35:44 +0100

    - Cleaned and Added yaml reading

--- src/main/scala/tutorial/BinanceWebSocketDemo.scala
+++ src/main/scala/tutorial/BinanceWebSocketDemo.scala
@@ -3,67 +3,50 @@ package tutorial
 import Models.BinanceBookTicker
 import Sources.BinanceWebSocketSource
 import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
-import org.apache.flink.api.common.functions.{AggregateFunction, MapFunction, RichFlatMapFunction, RichMapFunction}
-import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
-import org.apache.flink.configuration.Configuration
-import org.apache.flink.streaming.api.functions.sink.filesystem.{BucketAssigner, StreamingFileSink}
-import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
-import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.streaming.api.windowing.triggers.CountTrigger
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow
-import org.apache.flink.api.common.functions.MapFunction
 import org.apache.flink.api.common.serialization.SimpleStringEncoder
 import org.apache.flink.core.fs.Path
 import org.apache.flink.core.io.SimpleVersionedSerializer
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction
-import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
+import org.apache.flink.streaming.api.functions.sink.filesystem.{BucketAssigner, StreamingFileSink}
 import org.apache.flink.streaming.api.functions.sink.filesystem.bucketassigners.SimpleVersionedStringSerializer
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
-import org.apache.flink.types.Nothing
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.yaml.snakeyaml.Yaml
 
-import scala.collection.mutable.ArrayBuffer
-//import org.apache.flink.core.fs.Path
-//import org.apache.flink.formats.parquet.avro.ParquetAvroWriters
-//import org.apache.flink.formats.parquet.protobuf.ParquetProtoWriters
-//import org.apache.flink.streaming.api.functions.sink.filesystem.{RollingPolicy, StreamingFileSink}
-import org.apache.flink.util.Collector
+import java.io.FileInputStream
 import org.apache.flink.streaming.api.scala._
-//import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
-//import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy
+import org.apache.flink.util.Collector
 
-import java.time.Duration
-import java.util.Properties
-
+import java.time.format.DateTimeFormatter
+import java.time.{Duration, LocalDateTime}
 
 object BinanceWebSocketDemo {
 
-
   var half_life: Long = _
-  var sample_rate: Int = _
+  var sample_period: Long = _
+  var output_path: String = _
+  var symbol: String = _
 
   def main(args: Array[String]): Unit = {
 
-    //val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val confiFilePath = if (args.length > 0) args(0) else "src/main/resources/config.yml"
+
+    val config = readTheConfigFile(confiFilePath)
+
+    // create the stream execution environment with the webUI for monitoring on local
     val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI()
+    env.getCheckpointConfig.setCheckpointInterval(config.get("checkPoint_interval").asInstanceOf[Long])
 
-    // Read config file
-    val configPath = "config.properties"
-    val config = new Properties()
-
-    // Read configuration properties
-    config.load(getClass.getClassLoader.getResourceAsStream(configPath))
-
-    half_life = config.getProperty("half_life").toLong
-    sample_rate = config.getProperty("sample_period").toInt
-    val outputFolder = config.getProperty("output_path")
-    val symbol = config.getProperty("symbol")
     //val tickerStream: DataStream[BinanceBookTicker] = localFileSource(env, symbol)
     val tickerStream: DataStream[BinanceBookTicker] = liveBinanceSource(env, symbol)
 
-    val outputStream:DataStream[(String, Long, Double, Double)] = calculateDecaySum(tickerStream)
+    //calculatle the decay based on the sample rate
+    val outputStream: DataStream[(String, Long, Double, Double)] = calculateDecaySum(tickerStream, sample_period)
 
-    val csvFileSink = getStreamingFileSink(outputFolder)
+    val csvFileSink = getStreamingFileSink(output_path)
 
     outputStream.addSink(csvFileSink).name("symbols CSV sink")
     outputStream.addSink(new PrintSinkFunction[(String, Long, Double, Double)]()).name("print to console sink")
@@ -71,15 +54,22 @@ object BinanceWebSocketDemo {
     env.execute("Binance WebSocket Demo")
   }
 
-  //TODO: update the code to use window and produce output based on the sample
-  //TODO: update api call for multiple symbols
+  def readTheConfigFile(configFilePath: String): java.util.Map[String, Any] = {
+    // Read the YAML file
+    val input = new FileInputStream(configFilePath)
+    val yaml = new Yaml()
+    val data = yaml.load(input).asInstanceOf[java.util.Map[String, Any]]
 
-  //TODO: change the config file to YAML
+    // Get the properties from the YAML data
+    symbol = data.get("symbol").asInstanceOf[String]
+    output_path = data.get("output_path").asInstanceOf[String]
+    half_life = data.get("half_life").asInstanceOf[Long]
+    sample_period = data.get("sample_period").asInstanceOf[Long]
 
-  //TODO: Exactly once and why we cannot - add to the doc
-  //TODO: add the instructions to run it as well, flink-submit should work
+    data
+  }
 
-  def calculateDecaySum(tickerStream: DataStream[BinanceBookTicker]): DataStream[(String, Long, Double, Double)] = {
+  def calculateDecaySum(tickerStream: DataStream[BinanceBookTicker], sample_rate: Long): DataStream[(String, Long, Double, Double)] = {
     val watermarkStrategy = WatermarkStrategy
       .forBoundedOutOfOrderness[BinanceBookTicker](Duration.ofSeconds(3))
       .withTimestampAssigner(new SerializableTimestampAssigner[BinanceBookTicker] {
@@ -97,7 +87,7 @@ object BinanceWebSocketDemo {
       .window(TumblingEventTimeWindows.of(Time.milliseconds(sample_rate))) // window of required sample size in millisecond
       .process(new ProcessWindowFunction[BinanceBookTicker, (String, Long, Double, Double), String, TimeWindow] {
 
-        def process(key: String, context: Context, input: Iterable[BinanceBookTicker], out: Collector[(String,Long, Double, Double)]) = {
+        def process(key: String, context: Context, input: Iterable[BinanceBookTicker], out: Collector[(String, Long, Double, Double)]) = {
           var decaySumAsk = 0D
           var decaySumBid = 0D
           var bestAsk = 0D //todo: can price be negative?
@@ -168,91 +158,32 @@ object BinanceWebSocketDemo {
 
   }
 
-  def old() = {
-    //          //.process(new ProcessWindowFunction[BinanceBookTicker, (Long, Double, Double),String, TimeWindow] {
-    //
-    //            def process(key: String, context: Context, input: Iterable[BinanceBookTicker], out: Collector[(Long, Double, Double)]) = {
-    //              var decaySum = 0D
-    //
-    ////            //the math is actually pretty simple: take the previous timestamp and subtract the current timestamp.
-    ////            //divide it by the half life (which is a variable to be set) and multiply by 2. add the result to the price
-    ////            //and multiply by the previous exponential time decayed sum (which is the result of this function for the previous item)
-    //
-    //              var prevRecord:BinanceBookTicker = null
-    //
-    //              for (curRecord <- input) {
-    //                //decaySum = ((decaySum *  / half_life) + curRecord.askPrice) * prevRecord.askPrice
-    //                prevRecord = curRecord //first record will make the di zero because of the time difference
-    //                var di =Math.pow(2,(curRecord.eventTime - prevRecord.eventTime)/half_life)
-    //                decaySum = decaySum * di + curRecord.askPrice
-    //              }
-    //
-    //              out.collect((1,11,1))
-    //
-    //            }
-    //
-    //          })
-
-
-    //.process(new ProcessWindowFunction<Tuple2<Event, Integer>, String, Integer, TimeWindow>() {})
-
-    //    var processStream =
-    //      keyedStream
-    //        .window(TumblingEventTimeWindows.of(Time.of(1000))
-    //        .flatMap(new RichFlatMapFunction[BinanceBookTicker, (Long, Double, Double)] {
-    //
-    //          var prevRecordState: ValueState[(Long, Double)] = _
-    //
-    //          override def flatMap(curRecord: BinanceBookTicker, collector: Collector[(Long, Double, Double)]): Unit = {
-    //
-    //            // access the state value
-    //            val tmpPrevRecordState = prevRecordState.value
-    //
-    //            // If it hasn't been used before, it will be null
-    //            val prevRecord: (Long, Double) = if (tmpPrevRecordState != null) {
-    //              tmpPrevRecordState
-    //            } else {
-    //              (curRecord.eventTime, 1)
-    //            }
-    //
-    //            //the math is actually pretty simple: take the previous timestamp and subtract the current timestamp.
-    //            //divide it by the half life (which is a variable to be set) and multiply by 2. add the result to the price
-    //            //and multiply by the previous exponential time decayed sum (which is the result of this function for the previous item)
-    //            val timeDecaySum = ((2 * (curRecord.eventTime - prevRecord._1) / half_life) + curRecord.askPrice) * prevRecord._2
-    //
-    //            // update the state
-    //            prevRecordState.update((curRecord.eventTime, timeDecaySum))
-    //            //s"Diff: ${curRecord.eventTime - prevRecord._1}, TDS: ${timeDecaySum}"
-    //
-    //            collector.collect((curRecord.eventTime, curRecord.bidPrice, timeDecaySum))
-    //          }
-    //
-    //          override def open(parameters: Configuration): Unit = {
-    //            prevRecordState = getRuntimeContext.getState(
-    //              new ValueStateDescriptor[(Long, Double)]("prevRecord", createTypeInformation[(Long, Double)])
-    //            )
-    //          }
-    //
-    //        })
-    //
-    //    processStream
-  }
-
-  def getStreamingFileSink(outputFolder: String): StreamingFileSink[(String, Long, Double, Double)] ={
+  def getStreamingFileSink(outputFolder: String): StreamingFileSink[(String, Long, Double, Double)] = {
 
 
     // write the CSV stream to a file every 60 seconds
     val csvFileSink = StreamingFileSink
       .forRowFormat(new Path(outputFolder), new SimpleStringEncoder[(String, Long, Double, Double)]("UTF-8"))
-      .withRollingPolicy(DefaultRollingPolicy.builder().withRolloverInterval(60000L).build())
+      .withRollingPolicy(DefaultRollingPolicy.builder().withRolloverInterval(10000L).build())
       .withBucketAssigner(new BucketAssigner[(String, Long, Double, Double), String] {
-        override def getBucketId(element: (String, Long, Double, Double), context: BucketAssigner.Context): String = s"symbol=${element._1}"
-        //override def getSerializer: SimpleVersionedStringSerializer[String] = SimpleVersionedStringSerializer.INSTANCE
+        override def getBucketId(element: (String, Long, Double, Double), context: BucketAssigner.Context): String = {
+          s"symbol=${element._1}"
+          //          val pattern = "yyyy-MM-dd_HH"
+          //          // Get the current date and time
+          //          val now = LocalDateTime.now()
+          //
+          //          // Format the date and time according to the pattern
+          //          val formattedDateTime = now.format(DateTimeFormatter.ofPattern(pattern))
+          //
+          //          // Return the bucket id based on the formatted date and time
+          //          s"symbol=${element._1}/${formattedDateTime}"
+        }
         override def getSerializer: SimpleVersionedSerializer[String] = SimpleVersionedStringSerializer.INSTANCE
       })
       .build()
 
     csvFileSink
+
 
 
 
